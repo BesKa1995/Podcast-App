dd42253e71e02b2c357c7c54af9a8fc590d4a785
Beso Poladishvili <beskapoladishvili@gmail.com>
2022-07-21 20:50:51 +0400

    add localStorage for questions and load event to load all existing questions after refreshing

--- src/question.js
+++ src/question.js
@@ -4,8 +4,47 @@ export class Question {
       method: 'POST',
       body: JSON.stringify(question),
       headers: {
-        'Content-type': 'application/json'
+        'Content-Type': 'application/json'
       }
-    })
+    }).then(response => response.json())
+      .then(response => {
+        response.id = response.name
+        return question
+      })
+      .then(addToLocalStorage)
+      .then(Question.renderList)
   }
+  static renderList() {
+    const questions = getQuestionFromLocalStorage()
+
+    const html = questions.length
+      ? questions.map(toCard).join('')
+      : `<div class="mui--text-headline">You did not asked anything yet</div>`
+
+    const list = document.getElementById('list')
+    list.innerHTML = html
+  }
+
+}
+function addToLocalStorage(question) {
+  const all = getQuestionFromLocalStorage()
+  all.push(question)
+
+  localStorage.setItem('questions', JSON.stringify(all))
+}
+
+function getQuestionFromLocalStorage() {
+  return JSON.parse(localStorage.getItem('questions') || '[]')
+}
+
+function toCard(question) {
+  return `
+  <br>
+    <div class="mui--text-black-54">
+    ${new Date(question.date).toLocaleDateString()}
+    ${new Date(question.date).toLocaleTimeString()}    
+    </div>
+    <div>${question.text}</div>
+    <br>
+  `
 }
